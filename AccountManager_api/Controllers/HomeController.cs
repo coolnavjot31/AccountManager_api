89c9862b13ea3655f0e06f57f26fefe4362f805b
coolnavjot31 <coolnavjot31@gmail.com>
2023-01-19 22:18:27 +0530

    new commit
    new commit

---
+++ AccountManager_api/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AccountManager_api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
